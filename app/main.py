4959c2819d3d7945bcd9fe82b3d9dbfbea419272
Tucker Simpson <16.tuckersimpson@gmail.com>
2023-10-22 19:07:59 -0400

    Cleaned up repo and added gitignore

--- app/main.py
+++ app/main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+"""
+Goal today: initialize a couple of characters locally using langchain generative agents
+give characters local IDs
+publish API endpoints so users can get a response for prompts
+"""
 
 @app.get('/')
 async def index():
