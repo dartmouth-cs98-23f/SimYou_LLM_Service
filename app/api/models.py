acbc8499624384b0fab15354c5c786b0f4753e73
Tucker Simpson <16.tuckersimpson@gmail.com>
2024-02-04 15:47:30 -0500

    Updated scaffolding and models to reflect new API

--- app/api/models.py
+++ app/api/models.py
@@ -1,12 +1,16 @@
 import pydantic
+from types import List
 
 # Model for the post request used to prompt the agent
 class Prompt(pydantic.BaseModel):
-    sourceAgentID: str
-    targetAgentID: str
-    prompt: str
+    questionerID: str
+    responderID: str
+    msg: str
+    respondWithQuestion: bool
+    streamResponse: bool
     msgID=""
     responseID=""
+    convoID=""
 
 # Agent info object
 class AgentInfo:
@@ -18,4 +22,20 @@ class AgentInfo:
         self.firstName = firstName
         self.lastName = lastName
         self.description = description
-    
+
+class ConversationInfo(pydantic.BaseModel):
+    participant1ID: str
+    participant2ID: str
+    conversationID: str
+
+class InfoToInitAgent(pydantic.BaseModel):
+    agentID: str
+    questions: List[str]
+    answers: List[List[str]]
+
+class ThumbnailInfo(pydantic.BaseModel):
+    worldID: str
+    description: str
+
+
+
--- app/api/prompts.py
+++ app/api/prompts.py
@@ -2,27 +2,39 @@ from .models import AgentInfo
 from typing import List
 
 class Prompts:
-    def get_convo_prompt(message: str, targetAgentInfo: AgentInfo, sourceAgentInfo: AgentInfo, targetAgentMemories: List[str]):
-        memories_str = ""
-        for i in range(len(targetAgentMemories)):
-            memories_str += f"({i+1}) {targetAgentMemories[i]};\n"
+    def get_convo_prompt(message: str, responderInfo: AgentInfo, askerInfo: AgentInfo, relevantMems: List[str], recentMems: List[(str, str)]):
+        relevant_mems = ""
+        for i in range(len(relevantMems)):
+            relevant_mems += f"({i+1}) {relevantMems[i]};\n"
+        
+
+        #TODO: Assemble the recent memory from the list of chat summaries
+        recent_mems = ""
+        for i in range(len(recentMems)):
+            relevant_mems += f"({relevantMems[i][0]} said: \"{relevantMems[i][1]}\";\n"
         
         gpt_prompt = f"""
-        You are a character with the name {targetAgentInfo.firstName} {targetAgentInfo.lastName} this description:
-        \'{targetAgentInfo.description}\'
+        You are a character with the name {responderInfo.firstName} {responderInfo.lastName} this description:
+        \'{responderInfo.description}\'
         
         -----------------------------------------------------------------------------
 
-        You have these memories:
-        {memories_str}
+        You remember the following:
+        {relevant_mems}
         -----------------------------------------------------------------------------
 
-        Another character with the name {sourceAgentInfo.firstName} {sourceAgentInfo.lastName} has this description:
-        \'{sourceAgentInfo.description}\'
+        You are talking to another character with the name {askerInfo.firstName} {askerInfo.lastName} who has this description:
+        \'{askerInfo.description}\'
 
         -----------------------------------------------------------------------------
+        
+        This is what you have been talking about
 
-        {sourceAgentInfo.firstName} {sourceAgentInfo.lastName} says this to you:
+        {recent_mems}
+
+        ...
+
+        {askerInfo.firstName} {askerInfo.lastName} says this to you:
         \"{message}\" 
         
         -----------------------------------------------------------------------------
